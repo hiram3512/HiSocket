7261f8cf893f42ec20e93430262d22753cba7ed9
Hiram <hiramtan@live.com>
2016-06-21 18:15:56 +0800

    完成通信逻辑

--- unity/Assets/SocketClient.cs
+++ unity/Assets/SocketClient.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Net.Sockets;
+using UnityEngine;
 
 namespace HiTCP
 {
@@ -21,35 +22,87 @@ namespace HiTCP
         public SocketClient()
         {
             client = new TcpClient();
+            client.NoDelay = true;
             client.SendTimeout = client.ReceiveTimeout = timeOut;
             buffer = new byte[bufferSize];
         }
 
-        public void Connect(string paramIp, int paramPort)
+        public void Connect(string paramIp, int paramPort, Action paramEventHandler = null)
         {
             ip = paramIp;
             port = paramPort;
-
+            try
+            {
+                client.BeginConnect(ip, port, new AsyncCallback(delegate (IAsyncResult ar)
+                {
+                    try
+                    {
+                        TcpClient tempTcpClient = (TcpClient)ar.AsyncState;
+                        tempTcpClient.EndConnect(ar);
+                        if (paramEventHandler != null)
+                            paramEventHandler();
+                        client.Client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(Receive), client);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogError(e.ToString());
+                    }
+                }), client);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.ToString());
+            }
         }
 
         public void Send(byte[] param)
         {
-            throw new NotImplementedException();
+            client.Client.BeginSend(param, 0, param.Length, SocketFlags.None, new AsyncCallback(delegate (IAsyncResult ar)
+                 {
+                     try
+                     {
+                         TcpClient tempTcpClient = (TcpClient)ar.AsyncState;
+                         tempTcpClient.Client.EndSend(ar);
+                     }
+                     catch (Exception e)
+                     {
+                         Debug.LogError(e.ToString());
+                     }
+                 }), client);
         }
 
-        public void Receive()
+        public void Receive(IAsyncResult ar)
         {
-            throw new NotImplementedException();
+            try
+            {
+                TcpClient tempTcpClient = (TcpClient)ar.AsyncState;
+                int temp = tempTcpClient.Client.EndReceive(ar);
+                if (temp > 0)
+                {
+                    //handle buffer
+                    Array.Clear(buffer, 0, buffer.Length);
+                    client.Client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(Receive), client);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.ToString());
+            }
+
         }
 
         public void Close()
         {
-            throw new NotImplementedException();
+            if (IsConnected)
+            {
+                client.Close();
+                client = null;
+            }
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
