52b3cd34e1b051d4db216577174dea232b151a3a
Hiram <hiramtan@live.com>
2016-06-22 16:36:13 +0800

    添加数据处理逻辑

--- unity/Assets/Network/MsgHandler.cs
+++ unity/Assets/Network/MsgHandler.cs
@@ -3,6 +3,7 @@
 // Author: hiramtan@live.com
 //*********************************************************************
 
+using System;
 using System.Collections;
 using System.IO;
 
@@ -14,20 +15,62 @@ namespace HiSocket
         private Queue receiveQueue;
         private MemoryStream ms;
         private BinaryReader br;
-
         private long remainingBytesSize { get { return ms.Length - ms.Position; } }
+
+
+
+        public MsgHandler()
+        {
+            sendQueue = new Queue();
+            receiveQueue = new Queue();
+            ms = new MemoryStream();
+            br = new BinaryReader(ms);
+        }
+
+        public void Send(byte[] param)
+        {
+            sendQueue.Enqueue(param);
+            lock (this)
+            {
+                while (sendQueue.Count>0)
+                {
+                    //send
+                }
+            }
+        }
 
         public void Receive(byte[] paramBytes, int paramLength)
         {
             ms.Seek(0, SeekOrigin.End);
             ms.Write(paramBytes, 0, paramLength);
             ms.Seek(0, SeekOrigin.Begin);
-
-            while (remainingBytesSize>10)
+            while (remainingBytesSize > MsgDefine.Length)
             {
-                
+                UInt16 tempMsgLength = br.ReadUInt16();
+                ms.Position -= MsgDefine.Length;
+                if (remainingBytesSize >= tempMsgLength)
+                {
+                    byte[] tempBytes = br.ReadBytes(tempMsgLength);
+                    receiveQueue.Enqueue(tempBytes);
+                }
+                else
+                    break;
             }
+            byte[] tempLeftBytes = br.ReadBytes((int) remainingBytesSize);
+            ms.SetLength(0);
+            ms.Write(tempLeftBytes,0,tempLeftBytes.Length);
+            Receive();
         }
 
+        private void Receive()
+        {
+            lock (this)
+            {
+                while (receiveQueue.Count>0)
+                {
+                    //handle socket packge
+                }
+            }
+        }
     }
 }
