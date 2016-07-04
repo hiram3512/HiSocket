9ab39f40cce5669c0a461443ef997db49d512b10
Hiram <hiramtan@live.com>
2016-07-04 16:18:46 +0800

    添加消息处理和回调

---
+++ unity/Assets/Msg/MsgBase.cs
@@ -0,0 +1,18 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+
+namespace HiSocket
+{
+    public class MsgBase
+    {
+        public MsgBase(byte[] param)
+        {
+
+        }
+    }
+}
+
---
+++ unity/Assets/Msg/MsgManager.cs
@@ -0,0 +1,41 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HiSocket
+{
+    public class MsgManager
+    {
+        public delegate void MsgEventHandler(MsgBase param);
+        private static Dictionary<int, MsgEventHandler> msgDic = new Dictionary<int, MsgEventHandler>();
+
+
+        public static void Register(int paramKey, MsgEventHandler paramHandler)
+        {
+            if (msgDic.ContainsKey(paramKey))
+            {
+                Debug.LogWarning("dic already contain this key: " + paramKey);
+                return;
+            }
+            msgDic.Add(paramKey, paramHandler);
+        }
+
+        public static void ReceiveMsg(byte[] param)
+        {
+            int tempKey = BitConverter.ToUInt16(param, 0);
+            if (msgDic.ContainsKey(tempKey))
+            {
+                Debug.LogWarning("dic donnt contain this key: " + tempKey + "make sure you have register it in advance");
+                return;
+            }
+            MsgBase tempMsgBase = new MsgBase(param);
+            msgDic[tempKey](tempMsgBase);
+        }
+    }
+}
