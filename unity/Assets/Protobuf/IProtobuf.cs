e830793443a1e1d657e46b7c9098fea8c93c730a
Hiram <hriamtan@qq.com>
2017-05-11 11:26:36 +0800

    添加泛型单例（非多线程）

---
+++ unity/Assets/Protobuf/IProtobuf.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace HiSocket.Protobuf
+{
+    public interface IProtobuf
+    {
+        byte[] Serialize<T>(T param);
+
+        T Deserialize<T>(byte[] param);
+    }
+}
---
+++ unity/Assets/Singleton.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace HiSockt
+{
+    internal class Singleton<T> where T : new()
+    {
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new T();
+                return instance;
+            }
+        }
+    }
+}
