f27aa9e2c45538883cfa28427b01d9fc2877ec99
Hiram <hriamtan@qq.com>
2017-05-15 15:04:37 +0800

    添加多线程安全

--- unity/Assets/Singleton.cs
+++ unity/Assets/Singleton.cs
@@ -7,13 +7,22 @@ namespace HiSocket
     public class Singleton<T> where T : new()
     {
         private static T instance;
+        private static readonly object locker = new object();
 
         public static T Instance
         {
             get
             {
                 if (instance == null)
-                    instance = new T();
+                {
+                    lock (locker)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new T();
+                        }
+                    }
+                }
                 return instance;
             }
         }
