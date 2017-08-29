78fa6143c6f145e5b0fddb3c3b227f46c384e960
Hiram <hriamtan@qq.com>
2017-08-29 15:36:28 +0800

    修改基础逻辑

--- visual/HiSocket/HiSocket/ISocket.cs
+++ visual/HiSocket/HiSocket/ISocket.cs
@@ -1,14 +1,25 @@
-﻿namespace HiSocket
+﻿using System;
+
+namespace HiSocket
 {
     public interface ISocket
     {
-        int Buffer { set; }
+        int TimeOut { set; }
+        int ReceiveBufferSize { set; }
+
+        Action<SocketState> StateEvent { get; set; }
 
         bool IsConnected { get; }
         void Connect(string ip, int port);
         void DisConnect();
         void Send(byte[] bytes);
         long Ping();
-    }
 
+    }
+    public enum SocketState
+    {
+        Connected,
+        DisConnected,
+        Connecting,
+    }
 }
