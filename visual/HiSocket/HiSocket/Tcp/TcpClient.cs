729fed82911b60c3721a9bb8985d5a83f8a4cd61
Hiram <hriamtan@qq.com>
2017-10-13 17:48:20 +0800

    添加unity ping

--- visual/HiSocket/HiSocket/Tcp/TcpClient.cs
+++ visual/HiSocket/HiSocket/Tcp/TcpClient.cs
@@ -163,7 +163,29 @@ namespace HiSocket.Tcp
             System.Net.NetworkInformation.Ping tempPing = new System.Net.NetworkInformation.Ping();
             System.Net.NetworkInformation.PingReply temPingReply = tempPing.Send(ipAddress);
             return temPingReply.RoundtripTime;
-        }
+
+            //private int pingTime;
+            //private Ping p;
+            //private float timeOut = 1;
+            //private float lastTime;
+            //void Start()
+            //{
+            //    StartCoroutine(Ping());
+            //}
+            //IEnumerator Ping()
+            //{
+            //    p = new Ping("127.0.0.1");
+            //    lastTime = Time.realtimeSinceStartup;
+            //    while (!p.isDone && Time.realtimeSinceStartup - lastTime < 1)
+            //    {
+            //        yield return null;
+            //    }
+            //    pingTime = p.time;
+            //    p.DestroyPing();
+            //    yield return new WaitForSeconds(1);//一秒更新一次
+            //    StartCoroutine(Ping());
+            //    }
+    }
         
         private void ChangeState(SocketState state)
         {
