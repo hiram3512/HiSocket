083376387c6bf8d5e47cbb126b74734ca71474b0
Hiram <hriamtan@qq.com>
2017-12-29 17:50:23 +0800

    添加单元测试

---
+++ visual/HiSocket/UnitTest/TestPackage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HiSocket;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestPackage:IPackage
+    {
+        public void Unpack(IByteArray bytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pack(IByteArray bytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ visual/HiSocket/UnitTest/UnitConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HiSocket;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitConnection
+    {
+        [TestMethod]
+        public void TestTcpConnect()
+        {
+            TcpConnection connection = new TcpConnection(new TestPackage());
+            connection.StateChangeHandler = TestConnectHandler;
+           connection.Connect("",12);
+        }
+        [TestMethod]
+        public void TestUdpConnect()
+        {
+
+        }
+        [TestMethod]
+        public void TestConnectHandler(SocketState state)
+        {
+            Assert.IsTrue(state==SocketState.Connected);
+        }
+    }
+}
