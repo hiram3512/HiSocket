b4c715ac28d3b86d45cbfee715a3a90283696d32
Hiram <hriamtan@qq.com>
2018-01-30 17:57:26 +0800

    修改示例代码

--- unity/Assets/TestTcp.cs
+++ unity/Assets/TestTcp.cs
@@ -1,5 +1,6 @@
 ﻿//****************************************************************************
-// Description:
+// Description: only use tcp socket to send and receive message
+// Should achieve pack and unpack logic by yourself
 // Author: hiramtan@live.com
 //****************************************************************************
 
@@ -17,48 +18,43 @@ public class TestTcp : MonoBehaviour
         _tcp = new TcpConnection(_packer);
         _tcp.StateChangeEvent += OnState;
         _tcp.ReceiveEvent += OnReceive;
-
+        Connect();
+    }
+    void Connect()
+    {
         _tcp.Connect("127.0.0.1", 7777);
     }
-
     // Update is called once per frame
     void Update()
     {
         _tcp.Run();
     }
-
-    private void OnApplicationQuit()
-    {
-        _tcp.ReceiveEvent -= OnReceive;
-        _tcp.DisConnect();
-    }
-
-    private bool isStartSend;
     void OnState(SocketState state)
     {
         Debug.Log("current state is: " + state);
         if (state == SocketState.Connected)
         {
+            Send();
         }
     }
-
-    private int i;
-    void StartSend()
+    void Send()
     {
-        if (i > 10)
-            isStartSend = false;
-
-        Debug.Log(i);
-        var bytes = BitConverter.GetBytes(i);
-        _tcp.Send(bytes);
-        i++;
+        for (int i = 0; i < 10; i++)
+        {
+            var bytes = BitConverter.GetBytes(i);
+            Debug.Log("send message: " + i);
+            _tcp.Send(bytes);
+            i++;
+        }
     }
-
+    private void OnApplicationQuit()
+    {
+        _tcp.DisConnect();
+    }
     void OnReceive(byte[] bytes)
     {
         Debug.Log("receive bytes: " + BitConverter.ToInt32(bytes, 0));
     }
-
     public class Packer : IPackage
     {
         public void Unpack(IByteArray reader, out byte[] writer)
@@ -66,7 +62,6 @@ public class TestTcp : MonoBehaviour
             //get head length or id
             writer = reader.Read(reader.Length);
         }
-
         public void Pack(ref byte[] reader, IByteArray writer)
         {
             //add head length or id
