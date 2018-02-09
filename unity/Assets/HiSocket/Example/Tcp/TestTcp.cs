49d8d26c396240cb42adfe7d3870d90e1c368f1d
Hiram <hriamtan@qq.com>
2018-02-09 11:58:03 +0800

    delete source code and add dll to project

--- unity/Assets/HiSocket/Example/Tcp/TestTcp.cs
+++ unity/Assets/HiSocket/Example/Tcp/TestTcp.cs
@@ -53,32 +53,35 @@ public class TestTcp : MonoBehaviour
     }
     void OnReceive(byte[] bytes)
     {
-        Debug.Log("receive bytes: " + BitConverter.ToInt32(bytes, 0));
+        Debug.Log("receive bytes: " +bytes.Length);
     }
     public class Packer : IPackage
     {
         public void Unpack(IByteArray reader, Queue<byte[]> receiveQueue)
         {
             //get head length or id
-            while (reader.Length >= 1)
-            {
-                byte bodyLength = reader.Read(1)[0];
+            //while (reader.Length >= 1)
+            //{
+            //    byte bodyLength = reader.Read(1)[0];
 
-                if (reader.Length >= bodyLength)
-                {
-                    var body = reader.Read(bodyLength);
-                    receiveQueue.Enqueue(body);
-                }
-            }
+            //    if (reader.Length >= bodyLength)
+            //    {
+            //        var body = reader.Read(bodyLength);
+            //        receiveQueue.Enqueue(body);
+            //    }
+            //}
+
+            receiveQueue.Enqueue(reader.Read(reader.Length));
         }
         public void Pack(Queue<byte[]> sendQueue, IByteArray writer)
         {
             //add head length or id
-            byte[] head = new Byte[1] { 4 };
-            writer.Write(head, head.Length);
+            //byte[] head = new Byte[1] { 4 };
+            //writer.Write(head, head.Length);
+            //var body = sendQueue.Dequeue();
 
-            var body = sendQueue.Dequeue();
-            writer.Write(body, body.Length);
+            var test = sendQueue.Dequeue();
+            writer.Write(test, test.Length);
         }
     }
 }
