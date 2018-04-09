e8159858c1d49e6c27072b476490c5ff056eea53
Hiram <hriamtan@live.com>
2018-04-09 20:57:47 +0800

    单元测试:可变数组

---
+++ visual/HiSocket/NUnit.Tests/TestByteArray.cs
@@ -0,0 +1,25 @@
+﻿using HiSocket;
+using NUnit.Framework;
+using System.Text;
+
+namespace NUnit.Tests
+{
+    [TestFixture]
+    public class TestByteArray
+    {
+        [Test]
+        public void TestMethod()
+        {
+            IByteArray i = new ByteArray();
+            Assert.AreEqual(i.Length, 0);
+
+            byte[] hello = Encoding.UTF8.GetBytes("hello");
+            byte[] world = Encoding.UTF8.GetBytes("world");
+
+            i.Write(hello);
+            Assert.AreEqual(i.Length, 5);
+            var hello2 = i.Read(5);
+            Assert.AreEqual(hello, hello2);
+        }
+    }
+}
