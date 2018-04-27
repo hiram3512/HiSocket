ef0463838633db0bbf8ad6d040a4705cf2ea047a
Hiram <hriamtan@live.com>
2018-04-27 10:53:33 +0800

    add unity example

---
+++ unity/Assets/HiSocket/Example/HiSocketExample/TcpExample01.cs
@@ -0,0 +1,26 @@
+﻿/***************************************************************
+ * Description: 
+ *
+ * Documents: https://github.com/hiramtan/HiSocket_unity
+ * Author: hiramtan@live.com
+***************************************************************/
+
+using HiSocket;
+using System;
+
+namespace HiSocketExample
+{
+    class TcpExample01
+    {
+        static void Main(string[] args)
+        {
+            var tcp = new TcpConnection(new PackageExample());
+            tcp.OnReceive += Receive;
+            tcp.Send(new byte[1]);
+        }
+
+        static void Receive(byte[] bytes)
+        {
+        }
+    }
+}
