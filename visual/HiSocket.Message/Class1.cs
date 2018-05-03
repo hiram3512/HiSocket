c832c1e985c55ee5ac7e65d7ab5c50592b2f3ed8
Hiram <hriamtan@live.com>
2018-05-03 14:15:55 +0800

    add project to solution

---
+++ visual/HiSocket.Message/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HiSocket.Message
+{
+    public class Class1
+    {
+    }
+}
---
+++ visual/HiSocket.Udp/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HiSocket.Udp
+{
+    public class Class1
+    {
+    }
+}
